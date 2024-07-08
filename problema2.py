4dcde2dd66857392b2f16cb9af54d30fb3fc2fcf
Ariel Martin <arielmartin33@gmail.com>
2024-07-08 19:47:57 -0300

    first commit

---
+++ problema2.py
@@ -0,0 +1,11 @@
+# Obtencion de edades
+
+hermano1 = 13
+hermano2 = 10
+
+if hermano1 > hermano2:
+    print("El hermano1 es mayor que el hermano2")
+elif hermano2 > hermano1:
+    print("El hermano2 es mayor que hermano1")
+else:
+    print("los hermanos tienen la misma edad")
